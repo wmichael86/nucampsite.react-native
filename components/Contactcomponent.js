cbc0f0503da7cf0c21ec1f6e17d739eeb8230737
Michael Lewis <wmichael.lewis@outlook.com>
2020-11-18 04:47:11 -0800

    Workshop week 1 completed Early

--- components/Contactcomponent.js
+++ components/Contactcomponent.js
@@ -16,7 +16,8 @@ class Contact extends Component {
     render() { 
         return ( 
             <ScrollView>
-                <Card title='Contact Information' wrapperStyle={{margin: 20}}>
+                <Card title='Contact Information'
+                    wrapperStyle={{margin: 20}}>
                     <Text> 1 Nucamp Way</Text>
                     <Text> Seattle, WA 98001</Text>
                     <Text style={{marginBottom: 10}}> U.S.A.</Text>
