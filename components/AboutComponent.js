11eb62d1f65f4acb82660ef1d8f642fa95d45a12
Michael Lewis <wmichael.lewis@outlook.com>
2020-11-22 00:29:51 -0800

    Icons,Favorites, and Commnts (adding favorite heart :)

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -43,7 +43,7 @@ class About extends Component {
             );
         };
 
-        return ( 
+        return (
             <ScrollView>
                 <Mission />
                 <Card title='Community Partners'>
