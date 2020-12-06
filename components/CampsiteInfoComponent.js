f293302f1d91a443ba97a0f422f8b87e5769a499
Michael Lewis <wmichael.lewis@outlook.com>
2020-12-06 15:34:36 -0800

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
+    Alert, PanResponder, Share } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
@@ -63,6 +63,16 @@ function RenderCampsite(props) {
         }
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -95,6 +105,14 @@ function RenderCampsite(props) {
                         raised
                         reverse
                         onPress={() => props.onShowModal()}
+                    />
+                    <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
                     />
                 </View>
             </Card>
